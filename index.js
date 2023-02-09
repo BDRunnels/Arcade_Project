26e484594b5d28b503a60b52bbe45c4612ddefe0
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-02-09 15:15:20 -0700

    JS Updated - Current

--- index.js
+++ index.js
@@ -2,48 +2,75 @@
 
 
 
-// function makeContainer () {
-//     console.log("test")
-//     let rowsNeeded = 6
-//     while (rowsNeeded >= 1){
-//         let newElement = document.createElement("div")
-//         newElement.textContent = "text"
-//         newRow.prepend(newElement)
-//         newElement.classList.add("rowFlex")
-//         rowsNeeded--
 
-//     }
-//     newRow.style.display = "flex"
-// }
-
-// makeContainer()
 let mainSection = document.getElementById("main-section")
 let tableElement = document.getElementsByTagName("table")[0]
-const board = [];
+const board = [
+        [undefined, undefined, undefined], 
+        [undefined, undefined, undefined],
+        [undefined, undefined, undefined]
+    ];
 
-function buildGameBoard() {
-    for (let rowsSoFar = 0; rowsSoFar < 6; rowsSoFar++) {
-        board.push([rowsSoFar])
+// function buildGameBoard() {
+//     for (let rowsSoFar = 0; rowsSoFar < board.length; rowsSoFar++) {
+//         let currentElement = board[rowsSoFar]
+//         // console.log(currentElement)
 
-        let newBoardRow = document.createElement("tr");
+//         let newBoardRow = document.createElement("div");
 
-        newBoardRow.classList.add("boardRow")
-        newBoardRow.classList.add(rowsSoFar)
+//         newBoardRow.classList.add("boardRow")
+//         // newBoardRow.classList.add(rowsSoFar)
 
-        for (columnsSoFar = 0; columnsSoFar < 1; columnsSoFar++) {
-            let newBoardColumn = document.createElement("td")
-            newBoardColumn.classList.add("boardColumn")
-            newBoardColumn.classList.add(columnsSoFar)
+//         tableElement.appendChild(newBoardRow)
 
-            newBoardRow.appendChild(newBoardColumn)
+//         for (nestedI = 0; nestedI < currentElement.length; nestedI++) {
+//             let nestedElement = currentElement[nestedI]
+//             // console.log(nestedElement)
+//             let newNested = document.createElement("div");
+//             newNested.classList.add("column")
+//             newNested.classList.add(nestedI)
+            
+//             nestedElement.appendChild(newNested)
+//     }
+
+        // for (columnsSoFar = 0; columnsSoFar < 1; columnsSoFar++) {
+        //     let newBoardColumn = document.createElement("td")
+        //     newBoardColumn.classList.add("boardColumn")
+        //     newBoardColumn.classList.add(columnsSoFar)
+
+        //     newBoardRow.appendChild(newBoardColumn)
+        // }
+
+        
+//     }
+// } 
+
+
+function buildGameBoard () {
+    for (let i = 0; i < board.length; i++) {
+        let newBoardCell = document.createElement("div");
+
+        newBoardCell.classList.add("cell");
+        newBoardCell.classList.add(i)
+
+        tableElement.appendChild(newBoardCell);
+
+        for (let nestedI = 0; nestedI < board[i].length; nestedI++) {
+            let nestedElement = board[i][nestedI];
+            let newNestedCell = document.createElement("div")
+            console.log(nestedElement)
+
+            newNestedCell.classList.add(i)
+            newNestedCell.classList.add("nested")
+            newNestedCell.classList.add(nestedElement)
+            
+            newBoardCell.prepend(newNestedCell)
+
+            
         }
-
-        tableElement.appendChild(newBoardRow)
     }
-    
+
 }
-
-
 
 
 
